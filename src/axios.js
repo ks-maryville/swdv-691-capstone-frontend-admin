a643f139732bf35bf4c10a71ec2a18f9e91c3b1a
kevinsherrell <k.sherrell.1028@gmail.com>
2023-08-27 00:50:49 -0500

    update

--- src/axios.js
+++ src/axios.js
@@ -3,14 +3,14 @@ import axios from "axios";
 
 export const request = () => {
     return axios.create({
-        baseURL: "https://swdv-691-capstone-frontend-admin-2d0frnh8h-ks-maryville.vercel.app/",
+        baseURL: process.env.REACT_APP_DEFAULT_SERVER_URL,
         headers: {'Content-Type': "application/json"}
     })
 }
 
 export const requestWithToken = (token) => {
     return axios.create({
-        baseURL: "https://swdv-691-capstone-frontend-admin-2d0frnh8h-ks-maryville.vercel.app/",
+        baseURL: process.env.REACT_APP_DEFAULT_SERVER_URL,
         headers: {'Content-Type': 'application/json', "Authorization": `Bearer ${token}`}
     })
 }
